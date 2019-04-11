9687b4afa90c24bfc3465ae67c332020512c28e8
Jose Antonio <josesito@gmail.com>
2019-04-11 14:13:12 -0500

    Conexion con Microsoft Azure

--- conexion.php
+++ conexion.php
@@ -1,10 +1,15 @@
 <?php
 
     // Conexion a la base de datos
-	define('DB_HOST','localhost');
-	define('DB_USER','root');
-	define('DB_PASS','');
-	define('DB_NAME','crudapp');
+	define('DB_HOST','crudapp2019.mysql.database.azure.com');
+	define('DB_USER','josesitomyapp@crudapp2019');
+	define('DB_PASS','Josesito97');
+    define('DB_NAME','crudapp');
+    
+    //Database=crudapp;
+    //Data Source=crudapp2019.mysql.database.azure.com;
+    //User Id=josesitomyapp@crudapp2019;
+    //Password=Josesito97
 
 	# conectare la base de datos
     $con=@mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
